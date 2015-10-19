505eead1cc6234248d56778b68f9694f2e596115
hx0day <hx0day@hackerdom.ru>
2015-10-19 20:03:04 +0500

    fix(javascript-tasks-2): comment(http://stackoverflow.com/questions/8173210/delete-vs-splice-on-associative-array) @sameoldmadness

--- phoneBook.js
+++ phoneBook.js
@@ -1,36 +1,32 @@
 'use strict';
 
-var phoneBook = []; // Здесь вы храните записи как хотите
+var phoneBook = {}; // Здесь вы храните записи как хотите
 
-module.exports.isValidName = function isValidName(name) {
-    return (name.split(' ')
-        .length == 1 && /\s*[a-zA-Zа-яА-Я]+\s*/.test(name));
-};
+function isValidName(name) {
+    return /^[a-zа-яё]+$/i.test(name);
+}
 
-module.exports.isValidPhone = function isValidPhone(phone) {
-    var regString = '^(([\\+]?)?([\\d]{0,3})|([\\d]{1,2}))[\\s]?(\\(?([\\d]{3}\\))|';
-    regString += '[\\d]{3})[\\s|-]?[\\d]{3}[\\s|-]?[\\d]{1}[\\s|-]?[\\d]{3}';
-    var reg = new RegExp(regString);
-    return reg.test(phone);
-};
+function isValidPhone(phone) {
+    return /^(\+?(\d{1,3})|(\d{1,2}))\s?(\(?\d{3}\)|\d{3})[\s|-]?\d{3}[\s|-]?\d{1}[\s|-]?\d{3}/
+        .test(phone);
+}
 
-module.exports.isValidEmail = function isValidEmail(email) {
-    return /^[0-9A-zА-я\-_]{1,}@[0-9A-zА-я\-_]{1,}(\.[0-9A-zА-я\-_]{1,}){1,}/.test(email);
-};
-
+function isValidEmail(email) {
+    return /^[0-9A-z\-_]+@[0-9A-zА-яё\-_]+(\.[0-9A-zА-яё\-_]+)+/i.test(email);
+}
 /*
  Функция добавления записи в телефонную книгу.
  На вход может прийти что угодно, будьте осторожны.
  */
 module.exports.add = function add(name, phone, email) {
-    if (this.isValidName(name) && this.isValidPhone(phone) && this.isValidEmail(email)) {
-        phoneBook.push({
+    if (isValidName(name) && isValidPhone(phone) && isValidEmail(email)) {
+        var key = name + phone + email;
+        phoneBook[key] = {
             name: name,
             phone: phone,
-            email: email,
-            key: name + phone + email
-        });
-        var result = 'add ' + name + ', ' + phone + ', ' + email + '\n';
+            email: email
+        };
+        var result = 'add ' + name + ', ' + phone + ', ' + email;
         console.log(result);
     }
 };
@@ -40,27 +36,30 @@ module.exports.add = function add(name, phone, email) {
  Поиск ведется по всем полям.
  */
 module.exports.find = function find(query) {
-    for (var index in phoneBook) {
-        if (phoneBook[index].key.indexOf(query) >= 0) {
-            var result = 'find ' + phoneBook[index].name + ', ' + phoneBook[index].phone + ', ' +
-                phoneBook[index].email + '\n';
+    var keys = Object.keys(phoneBook);
+    keys.forEach(function (key) {
+        if (key.indexOf(query) >= 0) {
+            var result = 'find ' + phoneBook[key].name + ', ' + phoneBook[key].phone +
+                ', ' + phoneBook[key].email;
             console.log(result);
         }
-    }
+    });
 };
 
 /*
  Функция удаления записи в телефонной книге.
  */
 module.exports.remove = function remove(query) {
-    for (var index in phoneBook) {
-        if (phoneBook[index].key.indexOf(query) >= 0) {
-            var result = 'del ' + phoneBook[index].name + ', ' + phoneBook[index].phone + ', ' +
-                phoneBook[index].email + '\n';
+    var keys = Object.keys(phoneBook);
+    keys.forEach(function (key) {
+        if (key.indexOf(query) >= 0) {
+            var result = 'del ' + phoneBook[key].name + ', ' + phoneBook[key].phone +
+                ', ' +
+                phoneBook[key].email;
             console.log(result);
-            delete phoneBook[index];
+            delete phoneBook[key];
         }
-    }
+    });
 };
 
 /*
@@ -69,6 +68,8 @@ module.exports.remove = function remove(query) {
 module.exports.importFromCsv = function importFromCsv(filename) {
     var data = require('fs')
         .readFileSync(filename, 'utf-8');
+    var pattern = /\r\n|\r|\n/g;
+    var new_pattern = data.replace(pattern, '\n');
     var contactAll = data.split('\n');
     for (var i = 0; i < contactAll.length - 1; i++) {
         this.add.apply(this, contactAll[i].split(';'));
@@ -90,12 +91,14 @@ module.exports.showTable = function showTable() {
     var lengthName;
     var lengthEmail;
     var maxLengthTable;
-    var index;
 
-    for (index in phoneBook) {
-        lengthPhone = phoneBook[index].phone.length;
-        lengthName = phoneBook[index].name.length;
-        lengthEmail = phoneBook[index].email.length;
+    var keys = Object.keys(phoneBook);
+
+    keys.forEach(function (key) {
+
+        lengthPhone = phoneBook[key].phone.length;
+        lengthName = phoneBook[key].name.length;
+        lengthEmail = phoneBook[key].email.length;
         if (maxLengthPhone < lengthPhone) {
             maxLengthPhone = lengthPhone;
             collSpacePhone = maxLengthPhone - 7;
@@ -109,7 +112,7 @@ module.exports.showTable = function showTable() {
             collSpaceEmail = maxLengthEmail - 5;
 
         }
-    }
+    });
 
     maxLengthTable = (maxLengthPhone + maxLengthName + maxLengthEmail + 10);
     if (lengthTable < maxLengthTable) {
@@ -120,18 +123,19 @@ module.exports.showTable = function showTable() {
         '| email ' + ' '.repeat(collSpaceEmail) + '|');
     console.log('-'.repeat(lengthTable));
 
-    for (index in phoneBook) {
-        collSpacePhone = maxLengthPhone - phoneBook[index].phone.length;
-        collSpaceName = maxLengthName - phoneBook[index].name.length;
-        collSpaceEmail = maxLengthEmail - phoneBook[index].email.length;
-        console.log('| ' + phoneBook[index].name + ' '.repeat(collSpaceName) + ' | ' +
-            phoneBook[index].phone + ' '.repeat(collSpacePhone) + ' | ' + phoneBook[index].email +
+    keys.forEach(function (key) {
+        collSpacePhone = maxLengthPhone - phoneBook[key].phone.length;
+        collSpaceName = maxLengthName - phoneBook[key].name.length;
+        collSpaceEmail = maxLengthEmail - phoneBook[key].email.length;
+        console.log('| ' + phoneBook[key].name + ' '.repeat(collSpaceName) + ' | ' +
+            phoneBook[key].phone + ' '.repeat(collSpacePhone) + ' | ' + phoneBook[
+                key].email +
             ' '.repeat(collSpaceEmail) + ' |');
-    }
+    });
     console.log('-'.repeat(lengthTable));
 };
 
-String.prototype.repeat = function (n) {
+String.prototype.repeat = String.prototype.repeat || function (n) {
     return new Array(n + 1)
         .join(this);
 };
